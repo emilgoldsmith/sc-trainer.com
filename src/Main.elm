fce9b4481351a5c66557a1d50dca8fbd726a5e10
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2021-01-30 12:10:44 +0000

    Some basic state changing on key events

--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,126 @@
-module Main exposing (main)
+module Main exposing (view)
 
-import Components.Cube
+import Browser
+import Browser.Events as Events
 import Html exposing (..)
-import Models.Cube
+import Json.Decode as Decode
 
 
-main : Html Never
 main =
-    div [] [ Components.Cube.injectStyles, Components.Cube.view ]
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( BetweenTests, Cmd.none )
+
+
+type Model
+    = BetweenTests
+    | TestRunning
+    | EvaluateResult
+
+
+type Msg
+    = KeyUp Key
+    | KeyDown Key
+
+
+type Key
+    = Space
+    | AnythingElse
+    | W
+
+
+{-| Heavily inspired by <https://github.com/elm/browser/blob/1.0.2/notes/keyboard.md>
+-}
+decodeKey : Decode.Decoder Key
+decodeKey =
+    Decode.map toKey (Decode.field "key" Decode.string)
+
+
+toKey : String -> Key
+toKey keyString =
+    case keyString of
+        " " ->
+            Space
+
+        "w" ->
+            W
+
+        _ ->
+            AnythingElse
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    case model of
+        BetweenTests ->
+            Events.onKeyUp <| Decode.map KeyUp decodeKey
+
+        TestRunning ->
+            Sub.batch
+                [ Events.onKeyDown <|
+                    Decode.map KeyDown
+                        decodeKey
+                , Events.onMouseDown <|
+                    Decode.succeed
+                        (KeyDown AnythingElse)
+                ]
+
+        _ ->
+            Events.onKeyUp <| Decode.map KeyUp decodeKey
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case model of
+        BetweenTests ->
+            case msg of
+                KeyUp Space ->
+                    ( TestRunning, Cmd.none )
+
+                _ ->
+                    ( model, Cmd.none )
+
+        TestRunning ->
+            case msg of
+                KeyDown _ ->
+                    ( EvaluateResult, Cmd.none )
+
+                _ ->
+                    ( model, Cmd.none )
+
+        EvaluateResult ->
+            case msg of
+                KeyUp Space ->
+                    ( TestRunning, Cmd.none )
+
+                KeyUp W ->
+                    ( BetweenTests, Cmd.none )
+
+                _ ->
+                    ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    div [] [ viewState model ]
+
+
+viewState : Model -> Html Msg
+viewState model =
+    case model of
+        BetweenTests ->
+            text "Between Tests"
+
+        TestRunning ->
+            text "Test Running"
+
+        EvaluateResult ->
+            text "Evaluating Result"
