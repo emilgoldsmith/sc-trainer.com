dfad619b7627e02f0d63a941b4ab2dbf13a72d73
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2021-02-01 14:46:25 +0000

    Play around with state transitions and snackbars

--- src/Main.elm
+++ src/Main.elm
@@ -1,11 +1,16 @@
-module Main exposing (view)
+module Main exposing (main)
 
 import Browser
 import Browser.Events as Events
 import Html exposing (..)
+import Html.Attributes exposing (..)
 import Json.Decode as Decode
+import Process
+import Task
+import Time
 
 
+main : Program () Model Msg
 main =
     Browser.element
         { init = init
@@ -17,18 +22,28 @@ main =
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( BetweenTests, Cmd.none )
+    ( { trainerState = BetweenTests, snackbars = [] }, Cmd.none )
 
 
-type Model
+type alias Model =
+    { trainerState : TrainerState
+    , snackbars : List String
+    }
+
+
+type TrainerState
     = BetweenTests
     | TestRunning
-    | EvaluateResult
+    | EvaluateResult Time.Posix
 
 
 type Msg
     = KeyUp Key
     | KeyDown Key
+    | StopTest Time.Posix
+    | EvaluateWrong Time.Posix
+    | EvaluateCorrect Time.Posix
+    | DeleteOldestSnackbar
 
 
 type Key
@@ -59,7 +74,7 @@ toKey keyString =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    case model of
+    case model.trainerState of
         BetweenTests ->
             Events.onKeyUp <| Decode.map KeyUp decodeKey
 
@@ -79,48 +94,85 @@ subscriptions model =
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case model of
-        BetweenTests ->
-            case msg of
-                KeyUp Space ->
-                    ( TestRunning, Cmd.none )
+    case msg of
+        DeleteOldestSnackbar ->
+            ( { model | snackbars = List.take (List.length model.snackbars - 1) model.snackbars }, Cmd.none )
 
-                _ ->
-                    ( model, Cmd.none )
+        _ ->
+            case model.trainerState of
+                BetweenTests ->
+                    case msg of
+                        KeyUp Space ->
+                            ( { model | trainerState = TestRunning }, Cmd.none )
 
-        TestRunning ->
-            case msg of
-                KeyDown _ ->
-                    ( EvaluateResult, Cmd.none )
+                        _ ->
+                            ( model, Cmd.none )
 
-                _ ->
-                    ( model, Cmd.none )
+                TestRunning ->
+                    case msg of
+                        KeyDown _ ->
+                            ( model, Task.perform StopTest Time.now )
 
-        EvaluateResult ->
-            case msg of
-                KeyUp Space ->
-                    ( TestRunning, Cmd.none )
+                        StopTest timeStopped ->
+                            ( { model | trainerState = EvaluateResult timeStopped }, Cmd.none )
 
-                KeyUp W ->
-                    ( BetweenTests, Cmd.none )
+                        _ ->
+                            ( model, Cmd.none )
 
-                _ ->
-                    ( model, Cmd.none )
+                EvaluateResult timeStopped ->
+                    case msg of
+                        KeyUp Space ->
+                            ( model, Task.perform EvaluateCorrect Time.now )
+
+                        KeyUp W ->
+                            ( model, Task.perform EvaluateWrong Time.now )
+
+                        EvaluateCorrect timeEvaluated ->
+                            if moreThanASecondLater timeStopped timeEvaluated then
+                                ( { model | trainerState = BetweenTests }, Cmd.none )
+
+                            else
+                                addSnackbar model "correct too soon"
+
+                        EvaluateWrong timeEvaluated ->
+                            if moreThanASecondLater timeStopped timeEvaluated then
+                                ( { model | trainerState = BetweenTests }, Cmd.none )
+
+                            else
+                                addSnackbar model "wrong too soon"
+
+                        _ ->
+                            ( model, Cmd.none )
+
+
+addSnackbar : Model -> String -> ( Model, Cmd Msg )
+addSnackbar model snackbarText =
+    ( { model | snackbars = snackbarText :: model.snackbars }, Task.perform (always DeleteOldestSnackbar) (Process.sleep 3000) )
+
+
+moreThanASecondLater : Time.Posix -> Time.Posix -> Bool
+moreThanASecondLater first second =
+    Time.posixToMillis second - Time.posixToMillis first > 1000
 
 
 view : Model -> Html Msg
 view model =
-    div [] [ viewState model ]
+    div [] [ viewState model, div [ style "position" "fixed", style "top" "20px", style "left" "50%" ] <| List.map viewSnackbar model.snackbars ]
 
 
 viewState : Model -> Html Msg
 viewState model =
-    case model of
+    case model.trainerState of
         BetweenTests ->
             text "Between Tests"
 
         TestRunning ->
             text "Test Running"
 
-        EvaluateResult ->
-            text "Evaluating Result"
+        EvaluateResult timeStopped ->
+            text <| "Evaluating Result" ++ " " ++ (Time.toHour Time.utc timeStopped |> String.fromInt) ++ ":" ++ (Time.toMinute Time.utc timeStopped |> String.fromInt) ++ ":" ++ (Time.toSecond Time.utc timeStopped |> String.fromInt) ++ "." ++ (Time.toMillis Time.utc timeStopped |> String.fromInt)
+
+
+viewSnackbar : String -> Html Msg
+viewSnackbar snackbarText =
+    div [ style "border" "solid black 2px" ] [ text snackbarText ]
