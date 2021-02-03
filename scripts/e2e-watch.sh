b3cdc994c464481ac1badfa213fc7d1e8c6bf55c
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2021-02-03 00:03:18 +0000

    Setup most of the dev environment for cypress

---
+++ scripts/e2e-watch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euo pipefail
+
+ROOT_DIRECTORY=$(dirname "${BASH_SOURCE[0]}")/..
+
+cd $ROOT_DIRECTORY/end-to-end-tests
+
+./node_modules/.bin/cypress verify || ./node_modules/.bin/cypress install
+
+./node_modules/.bin/cypress open -P .
