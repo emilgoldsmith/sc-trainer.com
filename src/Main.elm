bb393b74319415a133f56d668a37ecd73e954fa3
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2021-03-27 22:20:24 +0000

    Group up the messages

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,4 @@
-module Main exposing (main)
+port module Main exposing (main)
 
 import AlgorithmRepository
 import Browser
@@ -30,6 +30,9 @@ main =
         , update = update
         , subscriptions = subscriptions
         }
+
+
+port logError : String -> Cmd msg
 
 
 init : ViewportSize -> ( Model, Cmd Msg )
@@ -73,16 +76,33 @@ type EvaluationMessage
 
 
 type Msg
-    = IgnoredKeyEvent
-    | StartTest TestStartData
-    | MillisecondsPassed Float
+    = GlobalMessage GlobalMsg
+    | BetweenTestsMessage BetweenTestsMsg
+    | TestRunningMessage TestRunningMsg
+    | EvaluateResultMessage EvaluateResultMsg
+
+
+type GlobalMsg
+    = WindowResized Int Int
+
+
+type BetweenTestsMsg
+    = StartTest TestStartData
+    | DoNothingBetweenTests
+
+
+type TestRunningMsg
+    = MillisecondsPassed Float
     | EndTest (Maybe Time.Posix)
-    | EndIgnoringKeyPressesAfterTransition
+
+
+type EvaluateResultMsg
+    = EndIgnoringKeyPressesAfterTransition
     | SpaceStarted
     | SpaceEnded
     | WStarted
     | WEnded
-    | WindowResized Int Int
+    | DoNothingEvaluateResult
 
 
 type TestStartData
@@ -152,145 +172,154 @@ subscriptions model =
         trainerSubscriptions =
             case model.trainerState of
                 BetweenTests _ ->
-                    Events.onKeyUp <|
-                        Decode.map
-                            (\key ->
-                                if key == Space then
-                                    StartTest NothingGenerated
+                    Sub.map BetweenTestsMessage <|
+                        Events.onKeyUp <|
+                            Decode.map
+                                (\key ->
+                                    if key == Space then
+                                        StartTest NothingGenerated
 
-                                else
-                                    IgnoredKeyEvent
-                            )
-                            decodeNonRepeatedKeyEvent
+                                    else
+                                        DoNothingBetweenTests
+                                )
+                                decodeNonRepeatedKeyEvent
 
                 TestRunning _ _ _ ->
-                    Sub.batch
-                        [ Events.onKeyDown <|
-                            Decode.map
-                                (always <| EndTest Nothing)
-                                decodeNonRepeatedKeyEvent
-                        , Events.onMouseDown <|
-                            Decode.succeed <|
-                                EndTest Nothing
-                        , Events.onAnimationFrameDelta MillisecondsPassed
-                        ]
-
-                EvaluatingResult { ignoringKeyPressesAfterTransition, spacePressStarted, wPressStarted } ->
-                    if ignoringKeyPressesAfterTransition then
-                        Sub.none
-
-                    else
+                    Sub.map TestRunningMessage <|
                         Sub.batch
                             [ Events.onKeyDown <|
                                 Decode.map
-                                    (\key ->
-                                        case key of
-                                            Space ->
-                                                SpaceStarted
-
-                                            W ->
-                                                WStarted
-
-                                            _ ->
-                                                IgnoredKeyEvent
-                                    )
+                                    (always <| EndTest Nothing)
                                     decodeNonRepeatedKeyEvent
-                            , Events.onKeyUp <|
-                                Decode.map
-                                    (\key ->
-                                        case key of
-                                            Space ->
-                                                if spacePressStarted then
-                                                    SpaceEnded
-
-                                                else
-                                                    IgnoredKeyEvent
-
-                                            W ->
-                                                if wPressStarted then
-                                                    WEnded
-
-                                                else
-                                                    IgnoredKeyEvent
-
-                                            _ ->
-                                                IgnoredKeyEvent
-                                    )
-                                    decodeNonRepeatedKeyEvent
+                            , Events.onMouseDown <|
+                                Decode.succeed <|
+                                    EndTest Nothing
+                            , Events.onAnimationFrameDelta MillisecondsPassed
                             ]
 
+                EvaluatingResult { ignoringKeyPressesAfterTransition, spacePressStarted, wPressStarted } ->
+                    Sub.map EvaluateResultMessage <|
+                        if ignoringKeyPressesAfterTransition then
+                            Sub.none
+
+                        else
+                            Sub.batch
+                                [ Events.onKeyDown <|
+                                    Decode.map
+                                        (\key ->
+                                            case key of
+                                                Space ->
+                                                    SpaceStarted
+
+                                                W ->
+                                                    WStarted
+
+                                                SomeKey _ ->
+                                                    DoNothingEvaluateResult
+                                        )
+                                        decodeNonRepeatedKeyEvent
+                                , Events.onKeyUp <|
+                                    Decode.map
+                                        (\key ->
+                                            case key of
+                                                Space ->
+                                                    if spacePressStarted then
+                                                        SpaceEnded
+
+                                                    else
+                                                        DoNothingEvaluateResult
+
+                                                W ->
+                                                    if wPressStarted then
+                                                        WEnded
+
+                                                    else
+                                                        DoNothingEvaluateResult
+
+                                                SomeKey _ ->
+                                                    DoNothingEvaluateResult
+                                        )
+                                        decodeNonRepeatedKeyEvent
+                                ]
+
         globalSubscriptions =
-            Events.onResize WindowResized
+            Events.onResize (\a b -> GlobalMessage (WindowResized a b))
     in
     Sub.batch [ trainerSubscriptions, globalSubscriptions ]
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        WindowResized width height ->
+update messageCategory model =
+    case ( messageCategory, model.trainerState ) of
+        ( GlobalMessage (WindowResized width height), _ ) ->
             ( { model | viewportSize = { width = width, height = height } }, Cmd.none )
 
-        _ ->
-            case model.trainerState of
-                BetweenTests _ ->
-                    case msg of
-                        StartTest NothingGenerated ->
-                            ( model, Random.generate (\alg -> StartTest (AlgGenerated alg)) generatePll )
+        ( BetweenTestsMessage msg, BetweenTests _ ) ->
+            Tuple.mapSecond (Cmd.map BetweenTestsMessage) <|
+                case msg of
+                    StartTest NothingGenerated ->
+                        ( model, Random.generate (\alg -> StartTest (AlgGenerated alg)) generatePll )
 
-                        StartTest (AlgGenerated alg) ->
-                            ( model, Task.perform (\time -> StartTest (EverythingGenerated alg time)) Time.now )
+                    StartTest (AlgGenerated alg) ->
+                        ( model, Task.perform (\time -> StartTest (EverythingGenerated alg time)) Time.now )
 
-                        StartTest (EverythingGenerated alg startTime) ->
-                            ( { model | trainerState = TestRunning startTime TimeInterval.zero alg }, Cmd.none )
+                    StartTest (EverythingGenerated alg startTime) ->
+                        ( { model | trainerState = TestRunning startTime TimeInterval.zero alg }, Cmd.none )
 
-                        _ ->
-                            ( model, Cmd.none )
+                    DoNothingBetweenTests ->
+                        ( model, Cmd.none )
 
-                TestRunning startTime intervalElapsed alg ->
-                    case msg of
-                        EndTest Nothing ->
-                            ( model, Task.perform (\time -> EndTest (Just time)) Time.now )
+        ( TestRunningMessage msg, TestRunning startTime intervalElapsed alg ) ->
+            case msg of
+                EndTest Nothing ->
+                    ( model, Task.perform (\time -> TestRunningMessage <| EndTest (Just time)) Time.now )
 
-                        EndTest (Just endTime) ->
-                            ( { model
-                                | trainerState =
-                                    EvaluatingResult
-                                        { spacePressStarted = False
-                                        , wPressStarted = False
-                                        , ignoringKeyPressesAfterTransition = True
-                                        , result = TimeInterval.betweenTimestamps { start = startTime, end = endTime }
-                                        }
-                                , expectedCube = model.expectedCube |> Cube.applyAlgorithm alg
-                              }
-                            , Task.perform (always EndIgnoringKeyPressesAfterTransition) (Process.sleep 100)
-                            )
+                EndTest (Just endTime) ->
+                    ( { model
+                        | trainerState =
+                            EvaluatingResult
+                                { spacePressStarted = False
+                                , wPressStarted = False
+                                , ignoringKeyPressesAfterTransition = True
+                                , result = TimeInterval.betweenTimestamps { start = startTime, end = endTime }
+                                }
+                        , expectedCube = model.expectedCube |> Cube.applyAlgorithm alg
+                      }
+                    , Task.perform (always <| EvaluateResultMessage EndIgnoringKeyPressesAfterTransition) (Process.sleep 100)
+                    )
 
-                        MillisecondsPassed timeDelta ->
-                            ( { model | trainerState = TestRunning startTime (TimeInterval.increment timeDelta intervalElapsed) alg }, Cmd.none )
+                MillisecondsPassed timeDelta ->
+                    ( { model | trainerState = TestRunning startTime (TimeInterval.increment timeDelta intervalElapsed) alg }, Cmd.none )
 
-                        _ ->
-                            ( model, Cmd.none )
+        ( EvaluateResultMessage msg, EvaluatingResult keyStates ) ->
+            Tuple.mapSecond (Cmd.map EvaluateResultMessage) <|
+                case msg of
+                    EndIgnoringKeyPressesAfterTransition ->
+                        ( { model | trainerState = EvaluatingResult { keyStates | ignoringKeyPressesAfterTransition = False } }, Cmd.none )
 
-                EvaluatingResult keyStates ->
-                    case msg of
-                        EndIgnoringKeyPressesAfterTransition ->
-                            ( { model | trainerState = EvaluatingResult { keyStates | ignoringKeyPressesAfterTransition = False } }, Cmd.none )
+                    SpaceStarted ->
+                        ( { model | trainerState = EvaluatingResult { keyStates | spacePressStarted = True } }, Cmd.none )
 
-                        SpaceStarted ->
-                            ( { model | trainerState = EvaluatingResult { keyStates | spacePressStarted = True } }, Cmd.none )
+                    WStarted ->
+                        ( { model | trainerState = EvaluatingResult { keyStates | wPressStarted = True } }, Cmd.none )
 
-                        WStarted ->
-                            ( { model | trainerState = EvaluatingResult { keyStates | wPressStarted = True } }, Cmd.none )
+                    SpaceEnded ->
+                        ( { model | trainerState = BetweenTests CorrectEvaluation }, Cmd.none )
 
-                        SpaceEnded ->
-                            ( { model | trainerState = BetweenTests CorrectEvaluation }, Cmd.none )
+                    WEnded ->
+                        ( { model | trainerState = BetweenTests WrongEvaluation }, Cmd.none )
 
-                        WEnded ->
-                            ( { model | trainerState = BetweenTests WrongEvaluation }, Cmd.none )
+                    DoNothingEvaluateResult ->
+                        ( model, Cmd.none )
 
-                        _ ->
-                            ( model, Cmd.none )
+        ( msg, trainerState ) ->
+            ( model
+            , logError
+                ("Message received during unexpected state"
+                    ++ Debug.toString msg
+                    ++ Debug.toString trainerState
+                )
+            )
 
 
 view : Model -> Html.Html Msg
@@ -302,45 +331,48 @@ viewFullScreen : Model -> Element Msg
 viewFullScreen model =
     case model.trainerState of
         BetweenTests message ->
-            el
-                [ testid "between-tests-container"
-                , width fill
-                , height fill
-                ]
-            <|
-                column [ centerX, centerY, spacing 50, padding 50 ]
-                    [ el [ centerX ] <| text "Between Tests"
-                    , el [ centerX ] <| viewEvaluationMessage message
-                    , Input.button
-                        [ testid "begin-button"
-                        , centerX
-                        , Background.color <| rgb255 0 128 0
-                        , padding 25
-                        , Border.rounded 15
-                        ]
-                        { onPress = Just <| StartTest NothingGenerated
-                        , label = text "Begin"
-                        }
+            Element.map BetweenTestsMessage <|
+                el
+                    [ testid "between-tests-container"
+                    , width fill
+                    , height fill
                     ]
+                <|
+                    column [ centerX, centerY, spacing 50, padding 50 ]
+                        [ el [ centerX ] <| text "Between Tests"
+                        , el [ centerX ] <| viewEvaluationMessage message
+                        , Input.button
+                            [ testid "begin-button"
+                            , centerX
+                            , Background.color <| rgb255 0 128 0
+                            , padding 25
+                            , Border.rounded 15
+                            ]
+                            { onPress = Just <| StartTest NothingGenerated
+                            , label = text "Begin"
+                            }
+                        ]
 
         TestRunning _ elapsedTime algTested ->
-            el
-                [ testid "test-running-container"
-                , width fill
-                , height fill
-                ]
-            <|
-                column
-                    [ centerX
-                    , centerY
-                    , spacing 50
+            Element.map BetweenTestsMessage <|
+                el
+                    [ testid "test-running-container"
+                    , width fill
+                    , height fill
                     ]
-                    [ el [ centerX ] <| displayTestCase model.viewportSize algTested
-                    , el [ testid "timer", centerX, Font.size (min model.viewportSize.height model.viewportSize.width // 5) ] <| text <| TimeInterval.displayOneDecimal elapsedTime
-                    ]
+                <|
+                    column
+                        [ centerX
+                        , centerY
+                        , spacing 50
+                        ]
+                        [ el [ centerX ] <| displayTestCase model.viewportSize algTested
+                        , el [ testid "timer", centerX, Font.size (min model.viewportSize.height model.viewportSize.width // 5) ] <| text <| TimeInterval.displayOneDecimal elapsedTime
+                        ]
 
         EvaluatingResult { result } ->
-            column [ testid "evaluate-test-result-container" ] [ text <| "Evaluating Result", displayTimeResult result, displayExpectedCubeState model.expectedCube ]
+            Element.map EvaluateResultMessage <|
+                column [ testid "evaluate-test-result-container" ] [ text <| "Evaluating Result", displayTimeResult result, displayExpectedCubeState model.expectedCube ]
 
 
 viewState : Model -> Element msg
