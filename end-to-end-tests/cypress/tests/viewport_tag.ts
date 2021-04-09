f18446fe46eba87b7ec74808c2334cb58a01b2e6
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2021-04-09 18:51:37 +0000

    Add viewport meta tag test

---
+++ end-to-end-tests/cypress/tests/viewport_tag.ts
@@ -0,0 +1,26 @@
+describe("has correct viewport properties on url:", function () {
+  const pathsToTest = ["", "/", "/index.html"];
+  // eslint-disable-next-line mocha/no-setup-in-describe
+  pathsToTest.forEach((path) =>
+    it("'" + path + "'", function () {
+      cy.visit(path);
+      cy.get('head meta[name="viewport"][content]').then((metaElements) => {
+        expect(metaElements).to.have.length(1);
+        const content = metaElements.attr("content");
+        if (content === undefined) {
+          throw new Error("content of meta element is undefined");
+        }
+        const expected = [
+          "width=device-width",
+          "height=device-height",
+          "initial-scale=1.0",
+        ];
+        const actual = content.split(",");
+        // Asserting equal not caring about order
+        expect(actual.length).to.equal(expected.length);
+        expect(actual).to.have.members(expected);
+        expect(expected).to.have.members(actual);
+      });
+    })
+  );
+});
