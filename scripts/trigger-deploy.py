191433c02e282ecb6d980dc1ae9de8b4b095afe8
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2021-04-16 22:16:39 +0000

    Figure out the webhook API and re-enable Cypress tests

--- scripts/trigger-deploy.py
+++ scripts/trigger-deploy.py
@@ -1,8 +1,9 @@
 import os
 import requests
 
-response = requests.post("https://cloudbuild.googleapis.com/v1/projects/carbide-ether-306312/triggers/deploy-main-branch-on-webhook:webhook", params={"secret": os.environ["SECRET"], "key": os.environ["API_KEY"]})
+response = requests.post("https://cloudbuild.googleapis.com/v1/projects/carbide-ether-306312/triggers/deploy-main-branch-on-webhook:webhook", params={"secret": os.environ["SECRET"], "key": os.environ["API_KEY"]}, json={})
+
 
 print(response.content.decode("utf-8"))
-if response.status_code < 200 or response.status_code >= 300:
-    raise Exception("Bad status code")
+if response.status_code != 200:
+    raise Exception("Bad status code " + str(response.status_code))
