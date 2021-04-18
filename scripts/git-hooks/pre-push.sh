6c5156eafc0454abc9a06d4289c39dfff4a7a3fe
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2021-04-18 18:45:07 +0000

    Update prepush

--- scripts/git-hooks/pre-push.sh
+++ scripts/git-hooks/pre-push.sh
@@ -6,8 +6,19 @@ CHECKS_DIRECTORY=$(dirname "${BASH_SOURCE[0]}")/../checks
 
 cd $CHECKS_DIRECTORY
 
-(output=$(git status --porcelain) && [ -z "$output" ]) || (echo "You have uncommitted changes. Commit or git stash them and try pushing again" && exit 1)
-elm make ../../src/Main.elm
+function run_check() {
+    local -a all_arguments=( "${@}" )
+
+    ("${all_arguments[@]:2}" && echo $1) || (echo $2 && exit 1)
+}
+
+function check_for_uncommitted_changes() {
+    output=$(git status --porcelain) && [ -z "$output" ]
+}
+
+run_check "No uncommitted changes" "You have uncommitted changes. Commit or git stash them and try pushing again" check_for_uncommitted_changes
+run_check "Compiled Successfully" "Compilation Failed" elm make ../../src/Main.elm
+rm --force index.html
 ./elm-analyse.sh
 ./elm-format.sh
 ./elm-verify-examples.sh
