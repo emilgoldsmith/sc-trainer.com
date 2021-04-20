d0f0c81de606d11a89916b0252dd3f420d3fcf4b
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2021-04-20 08:08:12 +0000

    Make run commands directory agnostic

--- scripts/run-hot.sh
+++ scripts/run-hot.sh
@@ -2,4 +2,8 @@
 
 set -euo pipefail
 
+ROOT_DIRECTORY=$(dirname "${BASH_SOURCE[0]}")/..
+
+cd $ROOT_DIRECTORY
+
 elm-live src/Main.elm --port 4000 --pushstate --hot --dir public -- --output=public/main.js --debug
