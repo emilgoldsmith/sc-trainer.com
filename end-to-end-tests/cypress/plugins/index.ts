cc2531b21a58cf3d806ac79c0ac8a49182c95943
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2021-05-12 10:17:28 +0000

    Fix cypress errors

--- end-to-end-tests/cypress/plugins/index.ts
+++ end-to-end-tests/cypress/plugins/index.ts
@@ -12,11 +12,9 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-/**
- * @type {Cypress.PluginConfig}
- */
-// eslint-disable-next-line no-unused-vars
-exports = () => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const pluginConfig: Cypress.PluginConfig = (_on, _config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 };
+export = pluginConfig;
