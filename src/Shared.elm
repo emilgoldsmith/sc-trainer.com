a2a4a7de15384839f748bcd676580f4671cd5bfb
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2021-05-25 19:40:57 +0000

    Fix up Shared.elm so it compiles

--- src/Shared.elm
+++ src/Shared.elm
@@ -7,9 +7,17 @@ module Shared exposing
     , update
     )
 
+import Browser.Events as Events
+import Element
+import Json.Decode as Decode
 import Request exposing (Request)
+import UI
+import ViewportSize exposing (ViewportSize)
+
+
 
 -- Flags
+
 
 type alias Flags =
     { viewportSize : { width : Int, height : Int }
@@ -24,33 +32,39 @@ type alias Flags =
         }
     }
 
+
+
 -- Model
 
+
 type alias Model =
-    { viewportSize : ViewportSize.ViewportSize
+    { viewportSize : ViewportSize
     , userHasKeyboard : Bool
     , userHasTouchScreen : Bool
     , palette : UI.Palette
     }
 
+
+
 -- Init
 
+
 init : Request -> Flags -> ( Model, Cmd Msg )
-init _ ({ viewportSize, userHasTouchScreen } ) =
+init _ { viewportSize, userHasTouchScreen } =
     let
-        builtViewportSize = ViewportSize.build viewportSize
+        builtViewportSize =
+            ViewportSize.build viewportSize
     in
-    (
-                { viewportSize = builtViewportSize
-                , userHasKeyboard = guessIfUserHasKeyboard {userHasTouchScreen = userHasTouchScreen} builtViewportSize
-                , userHasTouchScreen = userHasTouchScreen
-                , palette = UI.defaultPalette
-                }
+    ( { viewportSize = builtViewportSize
+      , userHasKeyboard = guessIfUserHasKeyboard { userHasTouchScreen = userHasTouchScreen } builtViewportSize
+      , userHasTouchScreen = userHasTouchScreen
+      , palette = UI.defaultPalette
       }
     , Cmd.none
     )
 
-guessIfUserHasKeyboard : { a | userHasTouchScreen : Bool} -> ViewportSize -> Bool
+
+guessIfUserHasKeyboard : { a | userHasTouchScreen : Bool } -> ViewportSize -> Bool
 guessIfUserHasKeyboard { userHasTouchScreen } viewportSize =
     let
         isLargeScreen =
@@ -75,32 +89,43 @@ guessIfUserHasKeyboard { userHasTouchScreen } viewportSize =
 
 -- Update
 
+
 type Msg
     = WindowResized Int Int
     | KeyboardWasUsed
+
+
 update : Request -> Msg -> Model -> ( Model, Cmd Msg )
 update _ msg model =
     Tuple.pair (updateModel msg model) Cmd.none
 
+
 updateModel : Msg -> Model -> Model
 updateModel msg model =
-                case msg of
-                    WindowResized width height ->
-                        let
-                            newViewportSize =ViewportSize.build {width = width, height = height}
-                        in
-                        { model |
-                            userHasKeyboard = guessIfUserHasKeyboard model newViewportSize, viewportSize = newViewportSize}
-                    KeyboardWasUsed ->
-                        { model | userHasKeyboard = True }
+    case msg of
+        WindowResized width height ->
+            let
+                newViewportSize =
+                    ViewportSize.build { width = width, height = height }
+            in
+            { model
+                | userHasKeyboard = model.userHasKeyboard || guessIfUserHasKeyboard model newViewportSize
+                , viewportSize = newViewportSize
+            }
+
+        KeyboardWasUsed ->
+            { model | userHasKeyboard = True }
+
+
 
 -- Subscriptions
+
 
 subscriptions : Request -> Model -> Sub Msg
 subscriptions _ model =
     Sub.batch
         [ Events.onResize WindowResized
-        , if context.userHasKeyboard then
+        , if model.userHasKeyboard then
             Sub.none
 
           else
