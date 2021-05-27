f9efa553eea73b3c5dec97792ffc400303878fa2
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2021-05-27 13:59:25 +0000

    Fix elm-analyse saying ElmSpa is an unused dependency

--- scripts/checks/elm-analyse.sh
+++ scripts/checks/elm-analyse.sh
@@ -6,4 +6,19 @@ ROOT_DIRECTORY=$(dirname "${BASH_SOURCE[0]}")/../..
 
 cd $ROOT_DIRECTORY
 
+TEMPORARY_MODULE="src/Temporary.elm"
+
+# Note this first one is a single arrow meaning overwrite, the next ones are double arrows which mean append
+
+# Also note that this must be a pretty proper module as we don't want Elm Analyse to error on it in any way
+# so we for example make sure there aren't any unused imports / variables etc.
+echo "module Temporary exposing (temporary)" > $TEMPORARY_MODULE
+echo "" >> $TEMPORARY_MODULE
+echo "import ElmSpa.Page" >> $TEMPORARY_MODULE
+echo "" >> $TEMPORARY_MODULE
+echo "temporary : effect -> { init : model, update : msg -> model -> model, view : view } -> ElmSpa.Page.Page shared route effect view model msg" >> $TEMPORARY_MODULE
+echo "temporary = ElmSpa.Page.sandbox" >> $TEMPORARY_MODULE
+
 elm-analyse
+
+rm src/Temporary.elm
