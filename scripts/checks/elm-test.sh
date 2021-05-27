110e3df1b54558ce91e9433113761d8f823072bc
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2021-05-27 15:09:20 +0000

    Make Elm Test work prior to elm-spa gen run

--- scripts/checks/elm-test.sh
+++ scripts/checks/elm-test.sh
@@ -6,4 +6,9 @@ ROOT_DIRECTORY=$(dirname "${BASH_SOURCE[0]}")/../..
 
 cd $ROOT_DIRECTORY
 
+# Elm Test complains if there are source-directories listed in elm.json that don't exist
+# so we create the .elm-spa ones in case they don't currently exist.
+# They are gitignored so shouldn't be a problem to leave them lying around
+mkdir -p .elm-spa/defaults .elm-spa/generated
+
 elm-test "$@"
