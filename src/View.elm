2038bf0aa1ab44036a825129fee595781b0430f1
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2021-05-27 10:38:48 +0000

    Add overlays to view

--- src/View.elm
+++ src/View.elm
@@ -8,6 +8,7 @@ type alias View msg =
     { pageSubtitle : Maybe String
     , body : Body msg
     , topLevelEventListeners : List (Element.Attribute msg)
+    , overlays : List (Element.Attribute msg)
     }
 
 
@@ -35,6 +36,7 @@ placeholder pageName =
     { pageSubtitle = Just pageName
     , body = WithNavigation <| Element.text pageName
     , topLevelEventListeners = []
+    , overlays = []
     }
 
 
@@ -43,19 +45,21 @@ none =
     { pageSubtitle = Nothing
     , body = Custom Element.none
     , topLevelEventListeners = []
+    , overlays = []
     }
 
 
 map : (a -> b) -> View a -> View b
-map fn { pageSubtitle, body, topLevelEventListeners } =
+map fn { pageSubtitle, body, topLevelEventListeners, overlays } =
     { pageSubtitle = pageSubtitle
     , body = mapBody fn body
     , topLevelEventListeners = List.map (Element.mapAttribute fn) topLevelEventListeners
+    , overlays = List.map (Element.mapAttribute fn) overlays
     }
 
 
 toBrowserDocument : View msg -> Browser.Document msg
-toBrowserDocument { pageSubtitle, body, topLevelEventListeners } =
+toBrowserDocument { pageSubtitle, body, topLevelEventListeners, overlays } =
     let
         appTitle =
             "Speedcubing Trainer"
@@ -69,11 +73,11 @@ toBrowserDocument { pageSubtitle, body, topLevelEventListeners } =
         List.singleton <|
             case body of
                 FullScreen element ->
-                    Element.layout (Element.inFront element :: topLevelEventListeners) Element.none
+                    Element.layout (Element.inFront element :: topLevelEventListeners ++ overlays) Element.none
 
                 WithNavigation element ->
-                    Element.layout topLevelEventListeners element
+                    Element.layout (topLevelEventListeners ++ overlays) element
 
                 Custom element ->
-                    Element.layout topLevelEventListeners element
+                    Element.layout (topLevelEventListeners ++ overlays) element
     }
