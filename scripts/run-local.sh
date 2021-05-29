4fe291ccec636bb9074aa8d29a77df8bab2160c1
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2021-05-29 17:49:49 +0000

    Realized it's better to just do elm-spa gen rather than elm-spa watch in run scripts

--- scripts/run-local.sh
+++ scripts/run-local.sh
@@ -8,6 +8,17 @@ cd $ROOT_DIRECTORY
 
 ./scripts/build-html.js --target=development
 
-elm-spa watch &
+elm-spa gen
+
+RESET_COLOUR=$(tput sgr0)
+RED=$(tput setaf 1)
+# The reason we don't use elm-spa watch & is because it rebuilds too often
+# and can cause some slightly annoying issues as that rebuild triggers extra
+# sometimes faulty rebuilds of elm-live if they aren't synced properly
+# even though there aren't any real changes to the Elm SPA code.
+echo
+echo "${RED}NOTE:${RESET_COLOUR}"
+echo "${RED}NOTE: Elm SPA generated files don't rebuild. Therefore a restart of the command is needed if files are removed/added/renamed in the Pages directory$RESET_COLOUR"
+echo "${RED}NOTE:${RESET_COLOUR}"
 
 elm-live src/Main.elm --port 4000 --pushstate --no-reload --dir build/public -- --output build/public/main.js
