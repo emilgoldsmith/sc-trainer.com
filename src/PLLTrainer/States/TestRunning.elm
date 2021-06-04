ead2a8c20db4e6e8e43d4308ae3889348e542777
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2021-06-04 10:04:30 +0000

    Make time pass on test running

--- src/PLLTrainer/States/TestRunning.elm
+++ src/PLLTrainer/States/TestRunning.elm
@@ -1,6 +1,7 @@
-module PLLTrainer.States.TestRunning exposing (Model, state)
+module PLLTrainer.States.TestRunning exposing (Model, Msg, state)
 
 import Algorithm
+import Browser.Events
 import Css exposing (htmlTestid, testid)
 import Cube
 import Element exposing (..)
@@ -14,24 +15,51 @@ import ViewCube
 import ViewportSize exposing (ViewportSize)
 
 
-state : Shared.Model -> TestCase -> { init : Model, view : Model -> StatefulPage.StateView msg }
-state { viewportSize } testCase =
+state :
+    Shared.Model
+    -> TestCase
+    -> (Msg -> msg)
+    ->
+        { init : Model
+        , view : Model -> StatefulPage.StateView msg
+        , update : Msg -> Model -> Model
+        , subscriptions : Sub msg
+        }
+state { viewportSize } testCase toMsg =
     { init = init
     , view = view viewportSize testCase
+    , update = update toMsg
+    , subscriptions = subscriptions toMsg
     }
 
 
 type alias Model =
-    TimeInterval
+    { elapsedTime : TimeInterval }
+
+
+type Msg
+    = MillisecondsPassed Float
 
 
 init : Model
 init =
-    TimeInterval.zero
+    { elapsedTime = TimeInterval.zero }
+
+
+update : (Msg -> msg) -> Msg -> Model -> Model
+update toMsg msg model =
+    case msg of
+        MillisecondsPassed timeDelta ->
+            { model | elapsedTime = TimeInterval.increment timeDelta model.elapsedTime }
+
+
+subscriptions : (Msg -> msg) -> Sub msg
+subscriptions toMsg =
+    Browser.Events.onAnimationFrameDelta (toMsg << MillisecondsPassed)
 
 
 view : ViewportSize -> TestCase -> Model -> StatefulPage.StateView msg
-view viewportSize testCase elapsedTime =
+view viewportSize testCase model =
     { topLevelEventListeners = View.buildTopLevelEventListeners []
     , overlays = View.buildOverlays []
     , body =
@@ -52,6 +80,6 @@ view viewportSize testCase elapsedTime =
                     ]
                   <|
                     text <|
-                        TimeInterval.displayOneDecimal elapsedTime
+                        TimeInterval.displayOneDecimal model.elapsedTime
                 ]
     }
