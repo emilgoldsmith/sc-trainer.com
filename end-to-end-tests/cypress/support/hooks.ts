8180aeb4d38f994d1edc6a3d605b04b20fa46263
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2021-06-07 04:24:33 +0000

    Allow feature flags to be modified in tests

--- end-to-end-tests/cypress/support/hooks.ts
+++ end-to-end-tests/cypress/support/hooks.ts
@@ -1,8 +1,36 @@
-/* eslint-disable mocha/no-top-level-hooks, mocha/no-hooks-for-single-case */
+/* eslint-disable mocha/no-top-level-hooks, mocha/no-exports, mocha/no-hooks-for-single-case */
 
-import { performStandardIntercepts } from "./interceptors";
+import {
+  addElmModelObserversAndModifiersToHtml,
+  addElmModelObserversAndModifiersToJavascript,
+  fixRandomnessSeedInJavascript,
+} from "./elm-monkey-patching";
+import { handleHtmlCypressModifications } from "./html-template-replacements";
+import {
+  ensureServerNotReloading,
+  HtmlModifier,
+  interceptHtml,
+  interceptJavascript,
+  JavascriptModifier,
+} from "./interceptors";
+
+const defaultHtmlModifiers: HtmlModifier[] = [
+  addElmModelObserversAndModifiersToHtml,
+  handleHtmlCypressModifications,
+];
+
+const defaultJavascriptModifiers: JavascriptModifier[] = [
+  addElmModelObserversAndModifiersToJavascript,
+  fixRandomnessSeedInJavascript,
+];
 
 beforeEach(function () {
   localStorage.setItem("plausible_ignore", "true");
-  performStandardIntercepts();
+  interceptHtml(...defaultHtmlModifiers);
+  interceptJavascript(...defaultJavascriptModifiers);
+  ensureServerNotReloading();
 });
+
+export function addHtmlModifier(modifier: HtmlModifier): void {
+  defaultHtmlModifiers.push(modifier);
+}
