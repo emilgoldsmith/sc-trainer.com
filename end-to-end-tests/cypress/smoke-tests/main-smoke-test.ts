621c7ac8cff142bab8cfbfcdba8f8a566c6d4f53
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2021-06-18 11:39:19 +0000

    [no-deploy] Attempt at fixing health check

--- end-to-end-tests/cypress/smoke-tests/main-smoke-test.ts
+++ end-to-end-tests/cypress/smoke-tests/main-smoke-test.ts
@@ -4,6 +4,6 @@ import { pllTrainerElements } from "tests/pll-trainer/state-and-elements.helper"
 describe("Main Smoke Test", function () {
   it("fetches the index without an error and displays the start page of the pll trainer", function () {
     cy.visit(paths.pllTrainer);
-    pllTrainerElements.startPage.assertAllShow();
+    pllTrainerElements.startPage.container.assertShows();
   });
 });
