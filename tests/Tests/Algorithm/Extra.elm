eaee0d71fd76a24e44f011dfff6fe854d4f117ad
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2021-09-03 09:08:35 +0000

    Extract out algorithm complexity to its own function

--- tests/Tests/Algorithm/Extra.elm
+++ tests/Tests/Algorithm/Extra.elm
@@ -1,4 +1,4 @@
-module Tests.Algorithm.Extra exposing (catpsTests)
+module Tests.Algorithm.Extra exposing (catpsTests, complexityTests)
 
 import Algorithm
 import Algorithm.Extra
@@ -11,10 +11,26 @@ import Test exposing (..)
 catpsTests : Test
 catpsTests =
     describe "complexityAdjustedTPS"
+        [ fuzz2 Fuzz.Extra.algorithm (Fuzz.floatRange 1 100000000) "it's just complexity per second" <|
+            \algorithm milliseconds ->
+                Algorithm.Extra.complexityAdjustedTPS { milliseconds = milliseconds } algorithm
+                    |> Expect.within (Expect.Absolute 0.0000000001)
+                        (Algorithm.Extra.complexity algorithm / (milliseconds / 1000))
+        , fuzz Fuzz.Extra.algorithmWithoutTPSIgnoredMoves "same algorithm executed in slower time should give lower catps" <|
+            \algorithm ->
+                Algorithm.Extra.complexityAdjustedTPS { milliseconds = 2000 } algorithm
+                    |> Expect.lessThan
+                        (Algorithm.Extra.complexityAdjustedTPS { milliseconds = 1000 } algorithm)
+        ]
+
+
+complexityTests : Test
+complexityTests =
+    describe "complexity"
         [ fuzz3 Fuzz.Extra.algorithm
             (Fuzz.tuple ( Fuzz.Extra.turnLength, Fuzz.Extra.turnDirection ))
             (Fuzz.tuple ( Fuzz.Extra.turnLength, Fuzz.Extra.turnDirection ))
-            "y rotations in start and beginning don't affect catps"
+            "y rotations in start and beginning don't affect complexity"
           <|
             \algorithm ( length1, direction1 ) ( length2, direction2 ) ->
                 let
@@ -34,24 +50,18 @@ catpsTests =
                 Algorithm.Extra.complexityAdjustedTPS { milliseconds = 1000 } withYRotationsAlgorithm
                     |> Expect.within (Expect.Absolute 0.0000001)
                         (Algorithm.Extra.complexityAdjustedTPS { milliseconds = 1000 } algorithm)
-        , fuzz Fuzz.Extra.algorithm "adding a turn with same time makes catps better (greater)" <|
+        , fuzz Fuzz.Extra.algorithm "adding a turn increases complexity" <|
             \algorithm ->
                 let
                     withTurnAdded =
                         Algorithm.append algorithm (Algorithm.fromTurnList [ Algorithm.Turn Algorithm.U Algorithm.Halfway Algorithm.Clockwise ])
                 in
-                Algorithm.Extra.complexityAdjustedTPS { milliseconds = 1000 } withTurnAdded
+                Algorithm.Extra.complexity withTurnAdded
                     |> Expect.greaterThan
-                        (Algorithm.Extra.complexityAdjustedTPS { milliseconds = 1000 } algorithm)
-        , fuzz Fuzz.Extra.algorithmWithoutTPSIgnoredMoves "same algorithm executed in slower time should give lower catps" <|
-            \algorithm ->
-                Algorithm.Extra.complexityAdjustedTPS { milliseconds = 2000 } algorithm
-                    |> Expect.lessThan
-                        (Algorithm.Extra.complexityAdjustedTPS { milliseconds = 1000 } algorithm)
-        , test "Satisfies the reference of a TPS, which is a U turn" <|
+                        (Algorithm.Extra.complexity algorithm)
+        , test "Satisfies the reference of 1 complexity, which is a U turn" <|
             \_ ->
-                Algorithm.Extra.complexityAdjustedTPS
-                    { milliseconds = 1000 }
+                Algorithm.Extra.complexity
                     (Algorithm.fromTurnList
                         [ Algorithm.Turn Algorithm.U Algorithm.OneQuarter Algorithm.Clockwise ]
                     )
