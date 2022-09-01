e77171f287f8d1577586dca0be0550225e765d04
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2022-09-01 13:06:35 +0000

    Have all pick target parameters tests be run

--- end-to-end-tests/cypress/e2e/pll-trainer.cy.ts
+++ end-to-end-tests/cypress/e2e/pll-trainer.cy.ts
@@ -35,7 +35,11 @@ describe("PLL Trainer", function () {
     describe("different paths in the app:", function () {
       it("shows new case for new user -> solved quickly + ending test by touching screen where correct button shows up doesn't press that button -> pick algorithm -> correct with goodjob text (no driller) -> same case again on a different visit, no new case page, doesn't display picker, doesn't display good job text", function () {
         cy.visit(paths.pllTrainer);
-        pickTargetParametersNavigateVariant1();
+        pickTargetParametersPageNoSideEffectsButScroll();
+        pickTargetParametersPageSideEffectsExceptNavigations();
+        testPickTargetParametersOnlySubmitsWithNoErrors(() =>
+          pllTrainerElements.pickTargetParametersPage.submitButton.get().click()
+        );
         newUserStartPageBeginNavigateVariant1();
         pllTrainerElements.newCasePage.container.assertShows();
         cy.clock();
@@ -282,7 +286,7 @@ describe("PLL Trainer", function () {
         pllTrainerElements.pickTargetParametersPage.targetTPSInput
           .get()
           .type("{selectall}{backspace}" + tps);
-        pickTargetParametersNavigateVariant1();
+        pickTargetParametersNavigateVariant2();
 
         assertItsNewUserNotRecurringUserStartPage();
 
@@ -296,7 +300,7 @@ describe("PLL Trainer", function () {
         pllTrainerElements.pickTargetParametersPage.targetTPSInput
           .get()
           .should("have.value", tps);
-        pickTargetParametersNavigateVariant2();
+        pickTargetParametersNavigateVariant3();
         newUserStartPageBeginNavigateVariant1();
         cy.clock();
         newCasePageNavigateVariant1();
@@ -1664,213 +1668,246 @@ describe("PLL Trainer", function () {
 function pickTargetParametersPageNoSideEffectsButScroll() {
   const elements = pllTrainerElements.pickTargetParametersPage;
 
-  ([
-    [
-      "looks right",
-      () => {
-        elements.explanation.assertConsumableViaVerticalScroll(
-          elements.container.specifier
-        );
-        elements.recognitionTimeInput.assertConsumableViaVerticalScroll(
-          elements.container.specifier
-        );
-        elements.targetTPSInput.assertConsumableViaVerticalScroll(
-          elements.container.specifier
-        );
-        elements.submitButton.assertConsumableViaVerticalScroll(
-          elements.container.specifier
-        );
-        cy.assertNoHorizontalScrollbar();
-      },
-    ],
-    [
-      "has correct default values",
-      () => {
-        elements.recognitionTimeInput.get().should("have.value", "2");
-        elements.targetTPSInput.get().should("have.value", "2.5");
-      },
-    ],
-    [
-      "displays decimal keyboard on mobile devices",
-      () => {
-        elements.recognitionTimeInput
-          .get()
-          .should("have.attr", "inputmode", "decimal");
-        elements.targetTPSInput
-          .get()
-          .should("have.attr", "inputmode", "decimal");
-      },
-    ],
-  ] as const).forEach(([testDescription, testFunction]) =>
-    cy.withOverallNameLogged({ message: testDescription }, testFunction)
+  cy.withOverallNameLogged(
+    { message: "pickTargetParametersPageNoSideEffectsButScroll" },
+    () => {
+      ([
+        [
+          "looks right",
+          () => {
+            elements.explanation.assertConsumableViaVerticalScroll(
+              elements.container.specifier
+            );
+            elements.recognitionTimeInput.assertConsumableViaVerticalScroll(
+              elements.container.specifier
+            );
+            elements.targetTPSInput.assertConsumableViaVerticalScroll(
+              elements.container.specifier
+            );
+            elements.submitButton.assertConsumableViaVerticalScroll(
+              elements.container.specifier
+            );
+            cy.assertNoHorizontalScrollbar();
+          },
+        ],
+        [
+          "has correct default values",
+          () => {
+            elements.recognitionTimeInput.get().should("have.value", "2");
+            elements.targetTPSInput.get().should("have.value", "2.5");
+          },
+        ],
+        [
+          "displays decimal keyboard on mobile devices",
+          () => {
+            elements.recognitionTimeInput
+              .get()
+              .should("have.attr", "inputmode", "decimal");
+            elements.targetTPSInput
+              .get()
+              .should("have.attr", "inputmode", "decimal");
+          },
+        ],
+      ] as const).forEach(([testDescription, testFunction]) =>
+        cy.withOverallNameLogged({ message: testDescription }, testFunction)
+      );
+    }
   );
 }
 
 function pickTargetParametersPageSideEffectsExceptNavigations() {
   const elements = pllTrainerElements.pickTargetParametersPage;
 
-  ([
-    [
-      "correctly inputs decimal inputs including converting commas to periods",
-      () => {
-        elements.recognitionTimeInput
-          .get()
-          .type("{selectall}{backspace}13.5", { delay: 0 })
-          .should("have.value", "13.5");
-        elements.targetTPSInput
-          .get()
-          .type("{selectall}{backspace}23.7", { delay: 0 })
-          .should("have.value", "23.7");
-        elements.recognitionTimeInput
-          .get()
-          .type("{selectall}{backspace}1,3", { delay: 0 })
-          .should("have.value", "1.3");
-        elements.targetTPSInput
-          .get()
-          .type("{selectall}{backspace}2,9", { delay: 0 })
-          .should("have.value", "2.9");
-      },
-    ],
-    [
-      "displays error exactly if there's an invalid number",
-      () => {
-        testInput(elements.recognitionTimeInput, elements.recognitionTimeError);
-        testInput(elements.targetTPSInput, elements.tpsError);
-
-        function testInput(
-          inputElement: OurElement,
-          expectedError: OurElement
-        ) {
-          inputElement
-            .get()
-            .type("{selectall}{backspace}abc", { delay: 0 })
-            .blur();
-          expectedError.assertShows();
-          inputElement
-            .get()
-            .type("{selectall}{backspace}3.5", { delay: 0 })
-            .blur();
-          expectedError.assertDoesntExist();
-          inputElement
-            .get()
-            .type("{selectall}{backspace}3.5.5", { delay: 0 })
-            .blur();
-          expectedError.assertShows();
-          inputElement
-            .get()
-            .type("{selectall}{backspace}61.1", { delay: 0 })
-            .blur();
-          expectedError.assertDoesntExist();
-          // Empty input should also error
-          inputElement
-            .get()
-            .type("{selectall}{backspace}", { delay: 0 })
-            .blur();
-          expectedError.assertShows();
-        }
-      },
-    ],
-    ...([
-      {
-        method: "submit button",
-        submit: () => elements.submitButton.get().click(),
-      },
-      {
-        method: "enter in recognition time input",
-        submit: () => elements.recognitionTimeInput.get().type("{enter}"),
-      },
-      {
-        method: "enter in TPS input",
-        submit: () => elements.targetTPSInput.get().type("{enter}"),
-      },
-    ] as const).map(
-      ({ method, submit }) =>
+  cy.withOverallNameLogged(
+    { message: "pickTargetParametersPageSideEffectsExceptNavigations" },
+    () => {
+      ([
         [
-          "doesn't submit if there are errors using " + method,
+          "correctly inputs decimal inputs including converting commas to periods",
           () => {
-            makeInvalid(
+            elements.recognitionTimeInput
+              .get()
+              .type("{selectall}{backspace}13.5", { delay: 0 })
+              .should("have.value", "13.5");
+            elements.targetTPSInput
+              .get()
+              .type("{selectall}{backspace}23.7", { delay: 0 })
+              .should("have.value", "23.7");
+            elements.recognitionTimeInput
+              .get()
+              .type("{selectall}{backspace}1,3", { delay: 0 })
+              .should("have.value", "1.3");
+            elements.targetTPSInput
+              .get()
+              .type("{selectall}{backspace}2,9", { delay: 0 })
+              .should("have.value", "2.9");
+          },
+        ],
+        [
+          "displays error exactly if there's an invalid number",
+          () => {
+            testInput(
               elements.recognitionTimeInput,
               elements.recognitionTimeError
             );
-            submit();
-            elements.container.assertShows();
-            makeInvalid(elements.targetTPSInput, elements.tpsError);
-            submit();
-            elements.container.assertShows();
-            makeValid(
-              elements.recognitionTimeInput,
-              elements.recognitionTimeError
-            );
-            submit();
-            elements.container.assertShows();
+            testInput(elements.targetTPSInput, elements.tpsError);
 
-            function makeInvalid(
+            function testInput(
               inputElement: OurElement,
-              errorElement: OurElement
-            ) {
-              inputElement.get().type("abc", { delay: 0 });
-              errorElement.waitFor();
-            }
-            function makeValid(
-              inputElement: OurElement,
-              errorElement: OurElement
+              expectedError: OurElement
             ) {
               inputElement
                 .get()
-                .type("{selectall}{backspace}2.0", { delay: 0 });
-              errorElement.assertDoesntExist();
+                .type("{selectall}{backspace}abc", { delay: 0 })
+                .blur();
+              expectedError.assertShows();
+              inputElement
+                .get()
+                .type("{selectall}{backspace}3.5", { delay: 0 })
+                .blur();
+              expectedError.assertDoesntExist();
+              inputElement
+                .get()
+                .type("{selectall}{backspace}3.5.5", { delay: 0 })
+                .blur();
+              expectedError.assertShows();
+              inputElement
+                .get()
+                .type("{selectall}{backspace}61.1", { delay: 0 })
+                .blur();
+              expectedError.assertDoesntExist();
+              // Empty input should also error
+              inputElement
+                .get()
+                .type("{selectall}{backspace}", { delay: 0 })
+                .blur();
+              expectedError.assertShows();
             }
           },
-        ] as const
-    ),
-  ] as const).forEach(([testDescription, testFunction]) =>
-    cy.withOverallNameLogged({ message: testDescription }, testFunction)
+        ],
+        ...([
+          {
+            method: "submit button",
+            submit: () => elements.submitButton.get().click(),
+          },
+          {
+            method: "enter in recognition time input",
+            submit: () => elements.recognitionTimeInput.get().type("{enter}"),
+          },
+          {
+            method: "enter in TPS input",
+            submit: () => elements.targetTPSInput.get().type("{enter}"),
+          },
+        ] as const).map(
+          ({ method, submit }) =>
+            [
+              "doesn't submit if there are errors using " + method,
+              () => {
+                makeInvalid(
+                  elements.recognitionTimeInput,
+                  elements.recognitionTimeError
+                );
+                submit();
+                elements.container.assertShows();
+                makeInvalid(elements.targetTPSInput, elements.tpsError);
+                submit();
+                elements.container.assertShows();
+                makeValid(
+                  elements.recognitionTimeInput,
+                  elements.recognitionTimeError
+                );
+                submit();
+                elements.container.assertShows();
+
+                function makeInvalid(
+                  inputElement: OurElement,
+                  errorElement: OurElement
+                ) {
+                  inputElement.get().type("abc", { delay: 0 });
+                  errorElement.waitFor();
+                }
+                function makeValid(
+                  inputElement: OurElement,
+                  errorElement: OurElement
+                ) {
+                  inputElement
+                    .get()
+                    .type("{selectall}{backspace}2.0", { delay: 0 });
+                  errorElement.assertDoesntExist();
+                }
+              },
+            ] as const
+        ),
+      ] as const).forEach(([testDescription, testFunction]) =>
+        cy.withOverallNameLogged({ message: testDescription }, testFunction)
+      );
+    }
   );
 }
 
 function pickTargetParametersNavigateVariant1() {
-  pllTrainerElements.pickTargetParametersPage.submitButton.get().click();
-  pllTrainerElements.pickTargetParametersPage.container.assertDoesntExist();
+  cy.withOverallNameLogged(
+    { message: "pickTargetParametersNavigateVariant1" },
+    () => {
+      pllTrainerElements.pickTargetParametersPage.submitButton.get().click();
+      pllTrainerElements.pickTargetParametersPage.container.assertDoesntExist();
+    }
+  );
 }
 
 function pickTargetParametersNavigateVariant2() {
-  pllTrainerElements.pickTargetParametersPage.recognitionTimeInput
-    .get()
-    .type("{enter}");
-  pllTrainerElements.pickTargetParametersPage.container.assertDoesntExist();
+  cy.withOverallNameLogged(
+    { message: "pickTargetParametersNavigateVariant2" },
+    () => {
+      pllTrainerElements.pickTargetParametersPage.recognitionTimeInput
+        .get()
+        .type("{enter}");
+      pllTrainerElements.pickTargetParametersPage.container.assertDoesntExist();
+    }
+  );
 }
 
 function pickTargetParametersNavigateVariant3() {
-  pllTrainerElements.pickTargetParametersPage.targetTPSInput
-    .get()
-    .type("{enter}");
-  pllTrainerElements.pickTargetParametersPage.container.assertDoesntExist();
+  cy.withOverallNameLogged(
+    { message: "pickTargetParametersNavigateVariant3" },
+    () => {
+      pllTrainerElements.pickTargetParametersPage.targetTPSInput
+        .get()
+        .type("{enter}");
+      pllTrainerElements.pickTargetParametersPage.container.assertDoesntExist();
+    }
+  );
 }
 
 function testPickTargetParametersOnlySubmitsWithNoErrors(submit: () => void) {
-  const elements = pllTrainerElements.pickTargetParametersPage;
+  cy.withOverallNameLogged(
+    { message: "testPickTargetParametersOnlySubmitsWithNoErrors" },
+    () => {
+      const elements = pllTrainerElements.pickTargetParametersPage;
 
-  makeInvalid(elements.recognitionTimeInput, elements.recognitionTimeError);
-  submit();
-  elements.container.assertShows();
-  makeInvalid(elements.targetTPSInput, elements.tpsError);
-  submit();
-  elements.container.assertShows();
-  makeValid(elements.recognitionTimeInput, elements.recognitionTimeError);
-  submit();
-  elements.container.assertShows();
-  makeValid(elements.targetTPSInput, elements.tpsError);
-  submit();
-  pllTrainerElements.newUserStartPage.container.assertShows();
+      makeInvalid(elements.recognitionTimeInput, elements.recognitionTimeError);
+      submit();
+      elements.container.assertShows();
+      makeInvalid(elements.targetTPSInput, elements.tpsError);
+      submit();
+      elements.container.assertShows();
+      makeValid(elements.recognitionTimeInput, elements.recognitionTimeError);
+      submit();
+      elements.container.assertShows();
+      makeValid(elements.targetTPSInput, elements.tpsError);
+      submit();
+      pllTrainerElements.newUserStartPage.container.assertShows();
 
-  function makeInvalid(inputElement: OurElement, errorElement: OurElement) {
-    inputElement.get().type("abc", { delay: 0 });
-    errorElement.waitFor();
-  }
-  function makeValid(inputElement: OurElement, errorElement: OurElement) {
-    inputElement.get().type("{selectall}{backspace}2.0", { delay: 0 });
-    errorElement.assertDoesntExist();
-  }
+      function makeInvalid(inputElement: OurElement, errorElement: OurElement) {
+        inputElement.get().type("abc", { delay: 0 });
+        errorElement.waitFor();
+      }
+      function makeValid(inputElement: OurElement, errorElement: OurElement) {
+        inputElement.get().type("{selectall}{backspace}2.0", { delay: 0 });
+        errorElement.assertDoesntExist();
+      }
+    }
+  );
 }
 
 function newUserStartPageNoSideEffectsButScroll() {
