972d4bc3b51c02c1cdccee2bb4f2f6475aef2a1f
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2023-04-01 22:19:04 +0000

    Add some new eslint rules

--- scripts/checks/eslint.sh
+++ scripts/checks/eslint.sh
@@ -6,4 +6,4 @@ E2E_DIRECTORY=$(dirname "${BASH_SOURCE[0]}")/../../end-to-end-tests
 
 cd $E2E_DIRECTORY
 
-yarn run eslint --max-warnings 0 cypress
+yarn run eslint --max-warnings 0 cypress --report-unused-disable-directives
