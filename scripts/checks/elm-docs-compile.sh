d865eda58c5c8567040f562e6d63426bcf91f886
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2023-04-06 21:55:11 +0000

    Make sure elm-review is checked in CI and on prepush

--- scripts/checks/elm-docs-compile.sh
+++ scripts/checks/elm-docs-compile.sh
@@ -6,4 +6,10 @@ ROOT_DIRECTORY=$(dirname "${BASH_SOURCE[0]}")/../..
 
 cd $ROOT_DIRECTORY
 
-./node_modules/.bin/elm-doc-preview -o /dev/null
+# It seems like sometimes it fails on first try and succeeds on second for some reason?
+# Or at the very least it seems to fail with bad error message on first try and then good one
+# on second. Either way, it can be a good idea to retry
+if ! ./node_modules/.bin/elm-doc-preview -o /dev/null; then
+    echo "RETRYING AFTER INITIAL MAYBE FALSE NEGATIVE FAILURE";
+    ./node_modules/.bin/elm-doc-preview -o /dev/null;
+fi;
